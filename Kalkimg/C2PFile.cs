b71c6cc365b4782284d57d08da3a8be982480748
The6P4C <watsonjcampbell@gmail.com>
2016-02-12 18:24:29 +1000

    Move methods from C2PFile class to Utility class

--- Kalkimg/C2PFile.cs
+++ Kalkimg/C2PFile.cs
@@ -1,18 +1,15 @@
-﻿using Ionic.Zlib;
-using System;
+﻿using System;
 using System.Diagnostics;
 using System.Drawing;
-using System.IO;
-using System.Reflection;
 
 namespace Kalkimg {
 	class C2PFile {
 		private byte[] fileData;
 
 		public C2PFile(Bitmap imageToConvert) {
-			byte[] imageData = BitmapToRGB565Data(imageToConvert);
+			byte[] imageData = Utility.BitmapToRGB565Data(imageToConvert);
 
-			byte[] compressedImageData = Compress(imageData);
+			byte[] compressedImageData = Utility.Compress(imageData);
 
 			byte[] header = GetHeader(compressedImageData.Length, imageToConvert.Width, imageToConvert.Height);
 			byte[] footer = GetFooter();
@@ -22,47 +19,6 @@ namespace Kalkimg {
 			Array.Copy(header, 0, fileData, 0, header.Length);
 			Array.Copy(compressedImageData, 0, fileData, header.Length, compressedImageData.Length);
 			Array.Copy(footer, 0, fileData, header.Length + compressedImageData.Length, footer.Length);
-		}
-
-		private static byte ConvertRange(byte originalStart, byte originalEnd, byte newStart, byte newEnd, byte value) {
-			double scale = (double) (newEnd - newStart) / (originalEnd - originalStart);
-			return (byte) (newStart + ((value - originalStart) * scale));
-		}
-
-		private static byte[] BitmapToRGB565Data(Bitmap image) {
-			byte[] imageData = new byte[image.Width * image.Height * 2];
-
-			int imageDataIndex = 0;
-
-			for (int y = 0; y < image.Height; ++y) {
-				for (int x = 0; x < image.Width; ++x) {
-					Color c = image.GetPixel(x, y);
-
-					byte r = ConvertRange(0, 255, 0, 0x1F, c.R);
-					byte g = ConvertRange(0, 255, 0, 0x3F, c.G);
-					byte b = ConvertRange(0, 255, 0, 0x1F, c.B);
-
-					int rgb565 = (r << 11) + (g << 5) + b;
-					byte rgb565_2 = (byte) (rgb565 >> 8 & 0xFF);
-					byte rgb565_1 = (byte) (rgb565 & 0xFF);
-
-					imageData[imageDataIndex] = rgb565_2;
-					imageData[imageDataIndex + 1] = rgb565_1;
-
-					imageDataIndex += 2;
-				}
-			}
-
-			return imageData;
-		}
-
-		private static byte[] Compress(byte[] input) {
-			using (MemoryStream ms = new MemoryStream()) {
-				using (ZlibStream zls = new ZlibStream(ms, CompressionMode.Compress, CompressionLevel.BestSpeed)) {
-					zls.Write(input, 0, input.Length);
-				}
-				return ms.ToArray();
-			}
 		}
 
 		public byte[] FileData {
---
+++ Kalkimg/Utility.cs
@@ -0,0 +1,48 @@
+﻿using Ionic.Zlib;
+using System.Drawing;
+using System.IO;
+
+namespace Kalkimg {
+	class Utility {
+		public static byte ConvertRange(byte originalStart, byte originalEnd, byte newStart, byte newEnd, byte value) {
+			double scale = (double) (newEnd - newStart) / (originalEnd - originalStart);
+			return (byte) (newStart + ((value - originalStart) * scale));
+		}
+
+		public static byte[] BitmapToRGB565Data(Bitmap image) {
+			byte[] imageData = new byte[image.Width * image.Height * 2];
+
+			int imageDataIndex = 0;
+
+			for (int y = 0; y < image.Height; ++y) {
+				for (int x = 0; x < image.Width; ++x) {
+					Color c = image.GetPixel(x, y);
+
+					byte r = ConvertRange(0, 255, 0, 0x1F, c.R);
+					byte g = ConvertRange(0, 255, 0, 0x3F, c.G);
+					byte b = ConvertRange(0, 255, 0, 0x1F, c.B);
+
+					int rgb565 = (r << 11) + (g << 5) + b;
+					byte rgb565_2 = (byte) (rgb565 >> 8 & 0xFF);
+					byte rgb565_1 = (byte) (rgb565 & 0xFF);
+
+					imageData[imageDataIndex] = rgb565_2;
+					imageData[imageDataIndex + 1] = rgb565_1;
+
+					imageDataIndex += 2;
+				}
+			}
+
+			return imageData;
+		}
+
+		public static byte[] Compress(byte[] input) {
+			using (MemoryStream ms = new MemoryStream()) {
+				using (ZlibStream zls = new ZlibStream(ms, CompressionMode.Compress, CompressionLevel.BestSpeed)) {
+					zls.Write(input, 0, input.Length);
+				}
+				return ms.ToArray();
+			}
+		}
+	}
+}
